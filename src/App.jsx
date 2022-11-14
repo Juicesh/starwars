b1fbbd9c63faf8bcb3ab18325a4538d0ec8bad25
Shyryn <shyryn.kusembay.03@mail.ru>
2022-11-14 10:06:11 +0600

    re init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Help from "./Help";
+import Main from './Main'
+
+export default function App() {
+ 
+
+  return (
+    // Routes
+      <Routes>
+        <Route element={<Main />} path="/" />
+        <Route element={<Help />} path="error" />
+        <Route path="*" element={<Navigate to="/error" replace />} />
+      </Routes>
+    
+  );
+}
